5cdcc29e9a37b9fbc8179a638297eb1a175ae600
Daniel <daniel@MacBook-Pro.local>
2017-08-28 12:46:20 -0400

    added all files for first commit

---
+++ Scripts/Deal20.js
@@ -0,0 +1,34 @@
+// JavaScript Document
+
+
+
+
+(function Deal20() {
+
+'use strict';
+	/*if (Deal20.imgArray[0] === 'undefined') {
+		
+		Deal20.imgArray = new Array(10);
+
+		for (var i = 0; i < Deal20.imgArray.length; i++) {
+			Deal20.imgArray[i] = i;
+		}
+	}
+ */
+	
+	document.getElementById("hi").innerHTML="8";
+	//document.getElementById("table1").cells[0].innerHTML= "2";
+	/*var table = document.getElementById("table1");
+	
+	for(var j=0, cell; cell=table.cells[j];j++){
+	var randImg = Math.floor(Math.random()*Deal20.imgArray.length);
+	}*/
+
+})();
+
+
+
+
+/*for(var i=0;i<imgArray.length;i++){
+	
+}*/
